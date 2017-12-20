cdac11158d3cb1cf81619dd47ea1e5374411aea9
Kingsley Gale-Sides <ktg25@mrao.cam.ac.uk>
2017-12-20 16:13:53 +0000

    Updated code to import fro uv files and with darwin paths

--- reducestrip_recipe.py
+++ reducestrip_recipe.py
@@ -1,17 +1,19 @@
 import os
 import glob
 from casarecipe import casatasks as c
+from casarecipe import repo
 import casac
 import casa
 import shutil
+import pyuvdata
 
 #File location specification
-DataDir="/mnt/ktg500/HeraIDR1"
-c.repo.REPODIR="/mnt/ktg500/repo"
+DataDir="/rds/project/bn204/rds-bn204-asterics/HERA/data"
+c.repo.REPODIR="/rds/project/bn204/rds-bn204-asterics/repo/strip"
 
 #processing specification
 Pol="xx" #or "yy", etc
-ProcessData=True
+ProcessData=False
 MakePNG=False
 RefAnt="53" #as a string
 
@@ -24,15 +26,36 @@ CalInDay=2458042
 #Target specification
 Target="J2000 17h45m40.0409s -29d0m28.118s"
 TargetCleanMask="box[[128pix,128pix],[896pix,896pix]]"
-InTimes = [12552,13298,14043] 
+InTimes = [13298] 
 InDay=2458042
 
 #string builders for various filenames
-InData = [ "zen." + str(InDay)+"." + str(x) + "." + str(Pol) + ".HH.uvfits" for x in InTimes]
-#InData = glob.glob(os.pahth.join(Datadir,"*.uvfits")) #process the whole directory
-CalFilename="zen." + str(CalInDay)+"." + str(CalTime) + "." + str(Pol) + ".HH.uvfits"
+#InData = [ os.path.join(DataDir, str(InDay), Pol,"zen." + str(InDay)+"." + str(x) + "." + str(Pol) + ".HH.uv") for x in InTimes]
 
+InData = glob.glob(os.path.join(DataDir,str(CalInDay),Pol,"*.uv")) #process the whole directory
+print InData
+CalFilename=os.path.join(DataDir,str(CalInDay),Pol,"zen." + str(CalInDay)+"." + str(CalTime) + "." + str(Pol) + ".HH.uv")
+print CalFilename
 ###############################
+def mkuvfits(fin):
+	hh=c.hf(mkuvfits, fin)
+	mm=repo.get(hh)
+	if mm:
+		c.trc( "[Cached]", "mkuvfits", fin)
+		return mm
+	else:
+		c.trc( "[Eval] ", "mkuvfits", fin, " -> ", hh)
+		UV = pyuvdata.UVData()
+		UV.read_miriad(fin,'miriad')
+		UV.phase_to_time(UV.time_array[0])
+		tempf=repo.mktemp()
+		os.remove(tempf)
+		UV.write_uvfits(tempf,'uvfits')
+	if not os.path.exists(tempf):
+		raise RuntimeError("No output produced by mkuvfits !")
+	return repo.put(tempf, hh)
+
+
 def mkinitmodel(tgt,modelname):
 	""" Initial model: just a point source in specified direction"""
 	if not os.path.exists(modelname):
@@ -58,7 +81,8 @@ def copyoutput(dsin,dsout,extension):
 def main():
 	cl=mkinitmodel(CalTarget,os.path.join(c.repo.REPODIR,"CL.cal"))
 	#for cal data
-	cms=c.importuvfits(CalFilename)
+	cfits=mkuvfits(CalFilename)
+	cms=c.importuvfits(cfits)
 	cmsf=c.flagdata(cms, autocorr=True)
 	cmsfr=c.fixvis(cmsf,phasecenter=CalTarget)
 	cmsfrm=c.ft(cmsfr,complist=cl,usescratch=True)
@@ -81,10 +105,11 @@ def main():
 				mask=CalCleanMask)
 
 	#for product data
-	ms=[c.importuvfits(f) for f in InData]
-	msf=[c.flagdata(f, autocorr=True) for f in ms]
-	msfc=[c.applycal(f, gaintable=[K, G]) for f in msf]
-	msfcs=[c.split(f,datacolumn="corrected") for f in msfc]
+	dfits=[mkuvfits(f) for f in InData]
+	dms=[c.importuvfits(f) for f in dfits]
+	dmsf=[c.flagdata(f, autocorr=True) for f in dms]
+	dmsfc=[c.applycal(f, gaintable=[K, G]) for f in dmsf]
+	dmsfcs=[c.split(f,datacolumn="corrected") for f in dmsfc]
 
 	#imgaing
 	msi=[c.clean(vis=f,
@@ -97,7 +122,7 @@ def main():
 				nterms=1,
 				spw='0:150~900',
 				reffreq='120MHz',
-				mask=TargetCleanMask) for f in msfcs]	
+				mask=TargetCleanMask) for f in dmsfcs]	
 	
 	#save the output images to something sensible
 	copyoutput(msi,InData,"img.image")
